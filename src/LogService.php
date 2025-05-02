cb679526816e96066e9efa8600c5796bf9762bbe
Silvanei Santos <silvanei.santos@objective.com.br>
2025-05-02 15:06:42 -0300

    Fix Expand/Colapse all button

--- src/LogService.php
+++ src/LogService.php
@@ -145,7 +145,7 @@ readonly class LogService
     private function formatContent(array $context, int $deep = 1, bool $isList = false): string
     {
         $button = <<<HTML
-        <button _="on click toggle .highlight-toggle-display on next .highlight-toggle then toggle .rotate-180 on first in me">
+        <button class="btn-toggle" _="on click toggle .highlight-toggle-display on next .highlight-toggle then toggle .rotate-180 on first in me">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" width="16">
                 <path 
                     fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" 

eaf5ee29775469226d1fe4ec234a29084a9d3a2e
Silvanei Santos <silvanei.santos@objective.com.br>
2025-05-25 17:39:10 -0300

    Add template engine

--- src/Controller/SearchAction.php
+++ src/Controller/SearchAction.php
@@ -9,6 +9,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use React\Http\Message\Response;
 use S3\Log\Viewer\ActionHandler;
 use S3\Log\Viewer\LogService;
+use S3\Log\Viewer\View\Search\SearchViewModel;
 use Throwable;
 
 readonly class SearchAction implements ActionHandler
@@ -23,9 +24,10 @@ readonly class SearchAction implements ActionHandler
             /** @var array{search?: string} $queryParams */
             $queryParams = $request->getQueryParams();
             $filter = $queryParams['search'] ?? '';
-            $html = $this->logService->search($filter);
+            $rows = $this->logService->search($filter);
+            $view = new SearchViewModel('search/log-entry', ['entries' => $rows]);
 
-            return new Response(200, ['Content-Type' => 'text/html'], $html);
+            return Response::html($view->render());
         } catch (Throwable $e) {
             return new Response(400, ['Content-Type' => 'text/html'], $e->getMessage());
         }
---
+++ src/View/GenericViewModel.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace S3\Log\Viewer\View;
+
+final readonly class GenericViewModel
+{
+    use TemplateEngine;
+}
